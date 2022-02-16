c72e36a47075d3e45f20b96ecb1ed1e893a06665
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-02-16 06:42:26 +0300

    create ManagerTemplate.hpp

---
+++ R3nzSkin/SDK/ManagerTemplate.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstddef>
+
+#include "Pad.hpp"
+
+template <class T>
+class ManagerTemplate {
+public:
+	PAD(0x4);
+	T** list;
+	std::size_t length;
+	std::size_t capacity;
+};
