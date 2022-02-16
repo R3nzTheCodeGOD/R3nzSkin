4f1769a3d2ee5e671ca9c724603c0638566ede4e
Yx-2 <97827402+Yx-2@users.noreply.github.com>
2022-02-16 15:04:21 +0800

    Use English Delete incomplete

--- lolskin_to_skin.py
+++ lolskin_to_skin.py
@@ -1,8 +1,6 @@
 import configparser
 import json
 import os
-# from json_merger import Merger
-# from json_merger.config import UnifierOps, DictMergerOps
 
 
 def skin_to_dictionary(path):
@@ -13,7 +11,6 @@ def skin_to_dictionary(path):
     for key, values in config.items():
         ini_dict[key] = dict(config.items(key))
         print(values)
-    # print(ini_dict)
     lolskin_hero_skin = ini_dict["SKIN_CHAMPION_ACTIVED"]
     del lolskin_hero_skin['custom_file']
 
@@ -28,20 +25,11 @@ def forward_data(lolskin_hero_skin: dict):
         key = key.capitalize()
         key = key + ".current_combo_skin_index"
         value = int(value) + int(1)
-        # + int(1)
-        # print(key,value)
         dict_from_list[key] = value
 
     return dict_from_list
 
 
-# def open_skin_json():
-#     skin_json = {}
-#     with open('py\R3nzSkin.json', 'r') as fp:
-#         skin_json = json.load(fp)
-
-#     # print(skin_json)
-#     return skin_json
 
 if __name__ == '__main__':
     LOLSKIN_CONFIG_PATH = r"C:\Fraps\data\My\Config.ini"
@@ -50,13 +38,10 @@ if __name__ == '__main__':
         hero_skin = skin_to_dictionary(LOLSKIN_CONFIG_PATH)
     else:
         LOLSKIN_CONFIG_PATH = input(
-            "lolskin配置文件 举例:\t" + LOLSKIN_CONFIG_PATH + "\n")
+            "lolskin Configuration file path Example:\t" + LOLSKIN_CONFIG_PATH + "\n")
     to_skin = forward_data(lolskin_hero_skin=hero_skin)
-    # print(to_skin)
 
     json_str = json.dumps(to_skin)
     print("JSON 对象：", json_str)
     with open('data.json', 'w', encoding="utf-8") as fp:
         json.dump(to_skin, fp)
-
-# TODO合并json文件
