9a806a388edc0b52ccbcd829c9e58eb078a724a2
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-02-16 06:40:35 +0300

    create AIMinionClient.hpp

---
+++ R3nzSkin/SDK/AIMinionClient.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "AIBaseCommon.hpp"
+
+class AIMinionClient : public AIBaseCommon {
+public:
+	AIBaseCommon* get_gold_redirect_target() noexcept
+	{
+		static const auto GetGoldRedirectTarget{ reinterpret_cast<AIBaseCommon*(__thiscall*)(void*)>(std::uintptr_t(::GetModuleHandle(nullptr)) + offsets::functions::GetGoldRedirectTarget) };
+		return GetGoldRedirectTarget(this);
+	}
+};
