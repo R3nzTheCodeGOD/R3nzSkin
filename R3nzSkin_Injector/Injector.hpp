223137605275b247721c9f450bf5a60e39ffd492
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-02-22 13:19:21 +0300

    Rename the injector at each start up.

--- R3nzSkin_Injector/Injector.hpp
+++ R3nzSkin_Injector/Injector.hpp
@@ -30,5 +30,7 @@ public:
 	static bool WINAPI isInjected(const std::uint32_t pid) noexcept;
 	static bool WINAPI inject(const std::uint32_t pid) noexcept;
 	static void WINAPI enableDebugPrivilege() noexcept;
+    static std::string randomString(std::uint32_t size) noexcept;
+    static void renameExe() noexcept;
 	static void run() noexcept;
 };
