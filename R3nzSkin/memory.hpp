6283cded96e0a6473921ceba0bc370e378dd62f8
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-04-01 17:04:47 +0300

    Update patterns to be much more stable (but patterns will break much more easily with incoming updates)

--- R3nzSkin/memory.hpp
+++ R3nzSkin/memory.hpp
@@ -5,7 +5,6 @@
 #include <d3d9.h>
 #include <d3d11.h>
 
-
 #include "Offsets.hpp"
 #include "SDK/AIBaseCommon.hpp"
 #include "SDK/AIHero.hpp"
@@ -45,12 +44,9 @@ private:
 	{
 		{
 			{
-				"A1 ? ? ? ? 56 83 78 08 00",
-				"A1 ? ? ? ? 68 ? ? ? ? 8B 70 08",
-				"A1 ? ? ? ? 83 78 08 02 0F 85 ? ? ? ?"
-			},
-			true, true, 0,
-			&offsets::global::GameClient
+				"A1 ? ? ? ? 56 83 78 08 00 75 ?",
+				"A1 ? ? ? ? 68 ? ? ? ? 8B 70 08 E8 ? ? ? ?"
+			}, true, true, 0, &offsets::global::GameClient
 		}
 	};
 
@@ -58,119 +54,84 @@ private:
 	{
 		{
 			{
-				"8B 3D ? ? ? ? 3B F7 75 09",
-				"A1 ? ? ? ? 8B 54 24 28 85 C0",
-				"8B 0D ? ? ? ? 85 C9 0F 84 ? ? ? ? 83 7E 10 00"
-			},
-			true, true, 0,
-			& offsets::global::Player
+				"8B 3D ? ? ? ? 3B F7 75 09 5F 5E",
+				"A1 ? ? ? ? 8B 54 24 28 85 C0 0F 84 ? ? ? ?"
+			}, true, true, 0, &offsets::global::Player
 		},
 		{
 			{
-				"A1 ? ? ? ? 8B 54 24 14 53",
-				"8B 0D ? ? ? ? 50 8D 44 24 18 C7 44 24 ? ? ? ? ?",
-				"8B 0D ? ? ? ? 6A 00 E8 ? ? ? ? 8B CE"
-			},
-			true, true, 0,
-			&offsets::global::ManagerTemplate_AIHero_
+				"A1 ? ? ? ? 8B 54 24 14 53 55",
+				"A1 ? ? ? ? 8B 70 04 8B 40 08 8D 3C 86 3B F7 74 ?"
+			}, true, true, 0, &offsets::global::ManagerTemplate_AIHero_
 		},
 		{
 			{
-				"89 1D ? ? ? ? 57 8D 4B 08",
-				"8B 3D ? ? ? ? 85 FF 74 2B 8B 4F 1C"
-			},
-			true, true, 0,
-			&offsets::global::ChampionManager
+				"89 1D ? ? ? ? 57 8D 4B 08 89 5C 24 ? C7 03 ? ? ? ? E8 ? ? ? ?",
+				"8B 3D ? ? ? ? 85 FF 74 2B 8B 4F 1C 8B 47 ? 3B C1"
+			}, true, true, 0, &offsets::global::ChampionManager
 		},
 		{
 			{
-				"A1 ? ? ? ? 53 55 8B 6C 24 1C",
-				"8B 35 ? ? ? ? 8B 56 04",
-				"8B 35 ? ? ? ? 8B 49 08 E8 ? ? ? ? F3 0F 10 0D ? ? ? ?"
-			},
-			true, true, 0,
-			&offsets::global::ManagerTemplate_AIMinionClient_
+				"A1 ? ? ? ? 53 55 8B 6C 24 1C 56 57 8B 78 04 8B 40 08",
+				"8B 35 ? ? ? ? 8B 56 04 8D 4E 04 8B 41 04 8D 04 82"
+			}, true, true, 0, &offsets::global::ManagerTemplate_AIMinionClient_
 		},
 		{
 			{
-				"3B 05 ? ? ? ? 75 72",
-				"A3 ? ? ? ? 6A 64 6A 00",
-				"FF 35 ? ? ? ? E8 ? ? ? ? 83 C4 0C 80 7E 2C 00"
-			},
-			true, true, 0,
-			&offsets::global::Riot__g_window
+				"3B 05 ? ? ? ? 75 72 8B 0D ? ? ? ? 85 C9",
+				"A3 ? ? ? ? 6A 64 6A 00 FF 15 ? ? ? ? 6A 50"
+			}, true, true, 0, &offsets::global::Riot__g_window
 		},
 		{
 			{
-				"8D 8E ? ? ? ? FF 74 24 4C"
-			},
-			false, true, 0,
-			&offsets::AIBaseCommon::CharacterDataStack
+				"8D 8E ? ? ? ? FF 74 24 4C",
+				"8D 8E ? ? ? ? FF 74 24 ? FF 74 24 ? E8 ? ? ? ? 8B 74 ? ? 85 F6 74 ? 8B C7 F0 0F C1 46 04"
+			}, false, true, 0, &offsets::AIBaseCommon::CharacterDataStack
 		},
 		{
 			{
-				"80 BE ? ? ? ? ? 75 4D 0F 31"
-			},
-			false, true, 0,
-			&offsets::AIBaseCommon::SkinId
+				"80 BE ? ? ? ? 00 75 4D 0F 31 33 C9 66 C7 86 ? ? ? ? ? ? 89 44 24 ? 33 FF 89 54 24 1C 66 0F 1F 44 00 00",
+			}, false, true, 0, &offsets::AIBaseCommon::SkinId
 		},
 		{
 			{
-				"8B B7 ? ? ? ? FF 70 28",
-				"8B 8F ? ? ? ? 88 A7 ? ? ? ?",
-				"8B 8E ? ? ? ? D1 E8",
-				"8B 86 ? ? ? ? 89 4C 24 08",
-				"8B 8E ? ? ? ? C1 E8 02",
-			},
-			false, true, 0,
-			&offsets::MaterialRegistry::D3DDevice
+				"8B B7 ? ? ? ? FF 70 28 8B 0E 56",
+				"8B 8F ? ? ? ? 88 A7 ? ? ? ? 0F B6 C4 8B 11"
+			}, false, true, 0, &offsets::MaterialRegistry::D3DDevice
 		},
 		{
 			{
-				"8B 95 ? ? ? ? 0F 10 44 24 ?",
-				"8D 9F ? ? ? ? 0F 84 ? ? ? ?",
-				"8B 8E ? ? ? ? 52 57"
-			},
-			false, true, 0,
-			&offsets::MaterialRegistry::SwapChain
+				"8D 9F ? ? ? ? 0F 84 ? ? ? ? 8B 0B 6A 00 6A 00 51  8B 01 FF 50 ? 85 C0",
+				"8B 95 ? ? ? ? 0F 10 44 24 ? 56 8B 02 66 0F 73 D8 ?"
+			}, false, true, 0, &offsets::MaterialRegistry::SwapChain
 		},
 		{
 			{
-				"83 EC 50 53 55 56 57 8B F9 8B 47 04"
-			},
-			true, false, 0,
-			&offsets::functions::CharacterDataStack__Push },
+				"83 EC 50 53 55 56 57 8B F9 8B 47 04 39 07"
+			}, true, false, 0, &offsets::functions::CharacterDataStack__Push },
 		{
 			{
-				"E8 ? ? ? ? 5F 5E 5B 83 C4 44",
-				"E8 ? ? ? ? 8B 4E 7C 5E"
-			},
-			true, false, 0,
-			&offsets::functions::CharacterDataStack__Update
+				"E8 ? ? ? ? 5F 5E 5B 83 C4 44 C2 04 00",
+				"83 EC 1C 56 57 8D 44 24 ? 8B F1 50 8D 44 24 ? 50 8D 44 24 ? 50 8D 44 24 ? 50 8D 44 24 ? 50 8D 44 24 ? 50"
+			}, true, false, 0, &offsets::functions::CharacterDataStack__Update
 		},
 		{
 			{
-				"E8 ? ? ? ? FF 73 58",
-				"E8 ? ? ? ? FF 75 3C",
-			},
-			true, false, 0,
-			&offsets::functions::Riot__Renderer__MaterialRegistry__GetSingletonPtr
+				"E8 ? ? ? ? FF 73 58 8B C8 8B 10 FF 52 ?",
+				"E8 ? ? ? ? 6A 00 6A 02 6A 00 8B 08 57 8B 51 ? 8B C8 FF D2 89 45 ?",
+			}, true, false, 0, &offsets::functions::Riot__Renderer__MaterialRegistry__GetSingletonPtr
 		},
 		{
 			{
-				"E8 ? ? ? ? 8B 0D ? ? ? ? 83 C4 04 8B F0 6A 0B",
-				"E8 ? ? ? ? 83 C4 04 8D 4E 0C 8B D0"
-			},
-			true, false, 0,
-			&offsets::functions::translateString_UNSAFE_DONOTUSE
+				"E8 ? ? ? ? 8B 0D ? ? ? ? 83 C4 04 8B F0 6A 0B E8 ? ? ? ? 56 50 55 57",
+				"E8 ? ? ? ? 83 C4 04 8D 4E 0C 8B D0 80 38 00 5E 74 0C"
+			}, true, false, 0, &offsets::functions::translateString_UNSAFE_DONOTUSE
 		},
 		{
 			{
-				"E8 ? ? ? ? 39 44 24 1C 5F",
-				"E8 ? ? ? ? 85 C0 74 3A"
-			},
-			true, false, 0,
-			&offsets::functions::GetGoldRedirectTarget
+				"E8 ? ? ? ? 39 44 24 1C 5F 5E 5D 0F 94 C0 5B",
+				"E8 ? ? ? ? 8B F8 8B 03 8B CB"
+			}, true, false, 0, &offsets::functions::GetGoldRedirectTarget
 		}
 	};
 };
