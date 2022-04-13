dfca88a5373618f8ca6a22806b075d3c2ae2ad5e
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-04-13 17:40:32 +0300

    Update the CharacterDataStack #87

--- R3nzSkin/SDK/CharacterStackData.hpp
+++ R3nzSkin/SDK/CharacterStackData.hpp
@@ -7,12 +7,8 @@
 
 class CharacterStackData {
 public:
-	AString model;
-	std::int32_t skin;
-	PAD(0x20);
-	bool update_spells;
-	bool dont_update_hud;
-	bool change_particle;
-	PAD(0x1);
-	PAD(0xC);
+    AString model;
+    PAD(0xC);
+    std::int32_t skin;
+    PAD(0x40);
 };
