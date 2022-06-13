f212a7721a8a9c9aa094adeedba7b0d7a0b8c094
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-06-13 22:44:02 +0300

    Write RandomGenerator

--- R3nzSkin/Utils.hpp
+++ R3nzSkin/Utils.hpp
@@ -1,115 +1,56 @@
 #pragma once
 
+#include <concepts>
+#include <mutex>
+#include <random>
+
 #include "imgui/imgui.h"
+
+class RandomGenerator {
+public:
+    template <std::integral T>
+    [[nodiscard]] static T random(T min, T max) noexcept
+    {
+        std::scoped_lock lock{ mutex };
+        return std::uniform_int_distribution{ min, max }(gen);
+    }
+
+    template <std::floating_point T>
+    [[nodiscard]] static T random(T min, T max) noexcept
+    {
+        std::scoped_lock lock{ mutex };
+        return std::uniform_real_distribution{ min, max }(gen);
+    }
+
+    template <typename T>
+    [[nodiscard]] static std::enable_if_t<std::is_enum_v<T>, T> random(T min, T max) noexcept
+    {
+        return static_cast<T>(random(static_cast<std::underlying_type_t<T>>(min), static_cast<std::underlying_type_t<T>>(max)));
+    }
+private:
+    inline static std::mt19937 gen{ std::random_device{}() };
+    inline static std::mutex mutex;
+};
+
+template <typename T>
+[[nodiscard]] T random(T min, T max) noexcept
+{
+    return RandomGenerator::random(min, max);
+}
 
 class KeyBind {
 public:
     enum KeyCode : unsigned char {
-        APOSTROPHE = 0,
-        COMMA,
-        MINUS,
-        PERIOD,
-        SLASH,
-        KEY_0,
-        KEY_1,
-        KEY_2,
-        KEY_3,
-        KEY_4,
-        KEY_5,
-        KEY_6,
-        KEY_7,
-        KEY_8,
-        KEY_9,
-        SEMICOLON,
-        EQUALS,
-        A,
-        ADD,
-        B,
-        BACKSPACE,
-        C,
-        CAPSLOCK,
-        D,
-        DECIMAL,
-        DEL,
-        DIVIDE,
-        DOWN,
-        E,
-        END,
-        ENTER,
-        F,
-        F1,
-        F10,
-        F11,
-        F12,
-        F2,
-        F3,
-        F4,
-        F5,
-        F6,
-        F7,
-        F8,
-        F9,
-        G,
-        H,
-        HOME,
-        I,
-        INSERT,
-        J,
-        K,
-        L,
-        LALT,
-        LCTRL,
-        LEFT,
-        LSHIFT,
-        M,
-        MOUSE1,
-        MOUSE2,
-        MOUSE3,
-        MOUSE4,
-        MOUSE5,
-        MULTIPLY,
-        MOUSEWHEEL_DOWN,
-        MOUSEWHEEL_UP,
-        N,
-        NONE,
-        NUMPAD_0,
-        NUMPAD_1,
-        NUMPAD_2,
-        NUMPAD_3,
-        NUMPAD_4,
-        NUMPAD_5,
-        NUMPAD_6,
-        NUMPAD_7,
-        NUMPAD_8,
-        NUMPAD_9,
-        O,
-        P,
-        PAGE_DOWN,
-        PAGE_UP,
-        Q,
-        R,
-        RALT,
-        RCTRL,
-        RIGHT,
-        RSHIFT,
-        S,
-        SPACE,
-        SUBTRACT,
-        T,
-        TAB,
-        U,
-        UP,
-        V,
-        W,
-        X,
-        Y,
-        Z,
-        LEFTBRACKET,
-        BACKSLASH,
-        RIGHTBRACKET,
-        BACKTICK,
-
-        MAX
+        APOSTROPHE = 0, COMMA, MINUS, PERIOD, SLASH, KEY_0, KEY_1, KEY_2,
+        KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, SEMICOLON, EQUALS,
+        A, ADD, B, BACKSPACE, C, CAPSLOCK, D, DECIMAL, DEL, DIVIDE, DOWN, E,
+        END, ENTER, F, F1, F10, F11, F12, F2, F3, F4, F5, F6, F7, F8, F9, G,
+        H, HOME, I, INSERT, J, K, L, LALT, LCTRL, LEFT, LSHIFT, M, MOUSE1, MOUSE2,
+        MOUSE3, MOUSE4, MOUSE5, MULTIPLY, MOUSEWHEEL_DOWN, MOUSEWHEEL_UP, N, NONE,
+        NUMPAD_0, NUMPAD_1, NUMPAD_2, NUMPAD_3, NUMPAD_4, NUMPAD_5, NUMPAD_6, NUMPAD_7,
+        NUMPAD_8, NUMPAD_9, O, P, PAGE_DOWN, PAGE_UP, Q, R, RALT, RCTRL, RIGHT, RSHIFT,
+        S, SPACE, SUBTRACT, T, TAB, U, UP, V, W, X, Y, Z, LEFTBRACKET, BACKSLASH,
+        RIGHTBRACKET, BACKTICK, MAX
     };
 
     KeyBind() = default;
