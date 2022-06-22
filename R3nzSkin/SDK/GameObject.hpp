c8e4c83dc4c19ffbe908899638038f37029584c3
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-06-22 05:30:44 +0300

    Add get_position() method

--- R3nzSkin/SDK/GameObject.hpp
+++ R3nzSkin/SDK/GameObject.hpp
@@ -4,9 +4,11 @@
 #include <string>
 
 #include "../offsets.hpp"
+#include "Vector.hpp"
 
 class GameObject {
 public:
 	[[nodiscard]] std::string& get_name() const noexcept { return *reinterpret_cast<std::string*>(std::uintptr_t(this) + offsets::GameObject::Name); }
 	[[nodiscard]] std::int32_t get_team() const noexcept { return *reinterpret_cast<std::int32_t*>(std::uintptr_t(this) + offsets::GameObject::Team); }
+	[[nodiscard]] Vector get_position() const noexcept { return *reinterpret_cast<Vector*>(std::uintptr_t(this) + offsets::GameObject::Position); }
 };
