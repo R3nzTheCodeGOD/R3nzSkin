122fce4ac87784961dc3a13bc520726be1457864
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-06-22 05:31:48 +0300

    Add get_visiblity() method

--- R3nzSkin/SDK/GameObject.hpp
+++ R3nzSkin/SDK/GameObject.hpp
@@ -11,4 +11,5 @@ public:
 	[[nodiscard]] std::string& get_name() const noexcept { return *reinterpret_cast<std::string*>(std::uintptr_t(this) + offsets::GameObject::Name); }
 	[[nodiscard]] std::int32_t get_team() const noexcept { return *reinterpret_cast<std::int32_t*>(std::uintptr_t(this) + offsets::GameObject::Team); }
 	[[nodiscard]] Vector get_position() const noexcept { return *reinterpret_cast<Vector*>(std::uintptr_t(this) + offsets::GameObject::Position); }
+	[[nodiscard]] bool get_visiblity() const noexcept { return *reinterpret_cast<bool*>(std::uintptr_t(this) + offsets::GameObject::Visible); }
 };
