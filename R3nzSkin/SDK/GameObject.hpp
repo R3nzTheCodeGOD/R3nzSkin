89ee7514ee3967296bf3ced7ee0a1e76ae530846
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-06-22 05:29:53 +0300

    Deprecate the get_id() method

--- R3nzSkin/SDK/GameObject.hpp
+++ R3nzSkin/SDK/GameObject.hpp
@@ -9,5 +9,4 @@ class GameObject {
 public:
 	[[nodiscard]] std::string& get_name() const noexcept { return *reinterpret_cast<std::string*>(std::uintptr_t(this) + offsets::GameObject::Name); }
 	[[nodiscard]] std::int32_t get_team() const noexcept { return *reinterpret_cast<std::int32_t*>(std::uintptr_t(this) + offsets::GameObject::Team); }
-	[[nodiscard]] std::int32_t get_id() const noexcept { return *reinterpret_cast<std::int32_t*>(std::uintptr_t(this) + offsets::GameObject::ID); }
 };
