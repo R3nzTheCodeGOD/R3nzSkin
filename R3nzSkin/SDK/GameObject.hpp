bf82b0006067c850d0ff195b7160614597826b31
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-06-25 14:28:51 +0300

    Add getBoundingRadius method to GameObject structure

--- R3nzSkin/SDK/GameObject.hpp
+++ R3nzSkin/SDK/GameObject.hpp
@@ -5,7 +5,9 @@
 
 #include "../offsets.hpp"
 #include "../imgui/imgui.h"
+#include "CallVirtual.hpp"
 #include "Vector.hpp"
+
 
 class GameObject {
 public:
@@ -13,5 +15,7 @@ public:
 	[[nodiscard]] std::int32_t get_team() const noexcept { return *reinterpret_cast<std::int32_t*>(std::uintptr_t(this) + offsets::GameObject::Team); }
 	[[nodiscard]] Vector get_position() const noexcept { return *reinterpret_cast<Vector*>(std::uintptr_t(this) + offsets::GameObject::Position); }
 	[[nodiscard]] bool get_visiblity() const noexcept { return *reinterpret_cast<bool*>(std::uintptr_t(this) + offsets::GameObject::Visible); }
+	[[nodiscard]] float getBoundingRadius() noexcept { return callVirtual<34, float>(this); }
+
 	[[nodiscard]] bool isOnScreen(const Vector& vec) const noexcept { return (((vec.x > 0) && vec.x < ImGui::GetIO().DisplaySize.x) && ((vec.y > 0) && vec.y < ImGui::GetIO().DisplaySize.y)); }
 };
