a74c5b14c7fbbae1dd627c99569f70dbb73b6a64
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2022-09-05 01:07:45 +0300

    Create database for #193

--- R3nzSkin/SkinDatabase.hpp
+++ R3nzSkin/SkinDatabase.hpp
@@ -23,6 +23,14 @@ public:
 		std::vector<const char*> skins;
 	};
 
+	class specialSkin {
+	public:
+		std::uint32_t champHash;
+		std::int32_t skinIdStart;
+		std::int32_t skinIdEnd;
+		std::vector<const char*> gears;
+	};
+
 	void load() noexcept;
 
 	std::map<std::uint32_t, std::vector<skin_info>> champions_skins;
@@ -79,5 +87,12 @@ public:
 			{ FNV("SRU_Razorbeak"), FNV("SRU_RazorbeakMini") },
 			{ "Razorbeak", "Chicken Razorbeak" }
 		}
+	};
+
+	std::vector<specialSkin> specialSkins{
+		{ FNV("Katarina"), 29, 36, { "Dagger 1", "Dagger 2", "Dagger 3", "Dagger 4", "Dagger 5", "Dagger 6" }},
+		{ FNV("Renekton"), 26, 32, { "Head off", "Head on", "Fins", "Ultimate" } },
+		{ FNV("MissFortune"), 16, 16, { "Scarlet fair", "Zero hour", "Royal arms", "Starswarm" } },
+		{ FNV("Ezreal"), 5, 5, { "Level 1", "Level 2", "Level 3" } }
 	};
 };
