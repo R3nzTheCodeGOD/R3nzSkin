e9b8f949106a20cd2db997f44dccde8b92f97521
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2023-04-25 22:37:25 +0300

    Add CallVirtual method #442

--- R3nzSkin/SDK/Pad.hpp
+++ R3nzSkin/SDK/Pad.hpp
@@ -20,3 +20,11 @@ public:
 { \
 	return reinterpret_cast<returnType*>(std::uintptr_t(this) + offset); \
 }
+
+template <std::size_t Index, typename ReturnType, typename... Args>
+ReturnType CallVirtual(std::uintptr_t instance, Args... args)
+{
+	using Fn = ReturnType(__fastcall*)(std::uintptr_t, Args...);
+	const auto function{ (*reinterpret_cast<Fn**>(instance))[Index] };
+	return function(instance, args...);
+}
