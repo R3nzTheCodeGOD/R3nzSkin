c7c61018f997a23556f322d57575135e95aefcec
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2024-01-10 17:41:35 +0300

    Update SkinId

--- R3nzSkin/memory.hpp
+++ R3nzSkin/memory.hpp
@@ -97,7 +97,7 @@ private:
 		},
 		{
 			{
-				"40 38 BB ? ? ? ? 0F 85 ? ? ? ? 66 C7 83 ? ? ? ? ? ? 0F 31 48 C1 E2 20 4C 8D 83 ? ? ? ? 48 0B C2 44 8B CF 48 89 44 24 ? 8B D7"
+				"40 38 BB ? ? 00 00 0F 85 ? ? 00 00 66 C7 83 ? ? ? ? ? ? 0F 31 48 C1 E2 20 4C 8D 83 ? ? 00 00 48 0B C2 44 8B CF 48 89 44 24 ? 8B D7 48 63 C2"
 			}, false, true, false, 0, &offsets::AIBaseCommon::SkinId
 		},
 		{
