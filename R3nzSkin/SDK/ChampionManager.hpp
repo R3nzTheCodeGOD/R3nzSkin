7feebb9557d8f62ce722c64885e21382975863d9
hyy2400 <62869308+hydy100@users.noreply.github.com>
2024-06-16 17:52:30 +0800

    Update ChampionManager.hpp
    14.12 Data Structure Update

--- R3nzSkin/SDK/ChampionManager.hpp
+++ R3nzSkin/SDK/ChampionManager.hpp
@@ -4,8 +4,9 @@
 
 #include "Champion.hpp"
 #include "Pad.hpp"
+#include "RiotArray.hpp"
 
 class ChampionManager {
 	PAD(0x18)
-	std::vector<Champion*> champions;
+	RiotArray<Champion*> champions;
 };
